4adcddb7eddeae07b120a3973ebde9d4b4d572d2
duyngoc0410 <duytnph54324@gmail.com>
2025-05-30 21:22:06 +0700

    commit project one

---
+++ src/main/java/vn/fpoly/workflow_demo_sum25/WorkflowDemoSum25Application.java
@@ -0,0 +1,13 @@
+package vn.fpoly.workflow_demo_sum25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkflowDemoSum25Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkflowDemoSum25Application.class, args);
+    }
+
+}
---
+++ src/test/java/vn/fpoly/workflow_demo_sum25/WorkflowDemoSum25ApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.fpoly.workflow_demo_sum25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkflowDemoSum25ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
